79bd068ef8e3d281ed94463eff966337f113f1c8
Arthur Courselle <arthur.courselle@epita.fr>
2025-05-04 13:10:27 +0200

    feat(transformer): Adding Attention mecanism and Encoder side

--- models/generative/transformer/transformer.py
+++ models/generative/transformer/transformer.py
@@ -1,4 +1,5 @@
 import math
+from typing import Callable, Tuple
 import torch
 from torch import nn
 from torch import Tensor
@@ -61,8 +62,8 @@ class LayerNorm(nn.Module):
         self.epsilon: float = epsilon
 
         # To allow the model to amplifies values when needed.
-        self.alpha = nn.Parameter(torch.ones(1))
-        self.bias = nn.Parameter(torch.zeros(1))
+        self.alpha: nn.Module = nn.Parameter(torch.ones(1))
+        self.bias: nn.Module = nn.Parameter(torch.zeros(1))
 
     def forward(self, X: Tensor) -> Tensor:
 
@@ -70,6 +71,136 @@ class LayerNorm(nn.Module):
         std = X.std(dim=-1, keepdim=True)
 
         return self.alpha * ((X - mean) / (std + self.epsilon)) + self.bias
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1) -> None:
+        super().__init__()
+
+        # self.conv1 = nn.Conv1d(d_model, d_ff, kernel_size=1)
+        # self.relu = nn.ReLU()
+        # self.conv2 = nn.Conv1d(d_ff, d_model, kernel_size=1)
+
+        self.l1: nn.Module = nn.Linear(d_model, d_ff)
+        self.dropout: nn.Module = nn.Dropout(dropout)
+        self.l2: nn.Module = nn.Linear(d_ff, d_model)
+
+    def forward(self, X: Tensor) -> Tensor:
+        return self.l2(self.dropout(torch.relu(self.l2(X))))  # (B, seq_len, d_model)
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: 0.1) -> None:
+        super().__init__()
+
+        self.d_model: int = d_model
+        self.h: int = h
+        self.dropout: nn.Module = nn.Dropout(dropout)
+
+        assert d_model % h == 0, "d_model must be divisible by h."
+
+        self.d_k: int = d_model // h
+
+        # Init Weights
+        self.w_q: nn.Module = nn.Linear(d_model, d_model)  # Query
+        self.w_k: nn.Module = nn.Linear(d_model, d_model)  # Key
+        self.w_v: nn.Module = nn.Linear(d_model, d_model)  # Value
+
+        self.w_o: nn.Module = nn.Linear(d_model, d_model)  # Output
+
+    def forward(self, Q: Tensor, K: Tensor, V: Tensor, mask: Tensor) -> Tensor:
+        Q = self.w_q(Q)  # (B, seq_len, d_model)
+        K = self.w_k(K)  # (B, seq_len, d_model)
+        V = self.w_v(V)  # (B, seq_len, d_model)
+
+        Q = Q.view(Q.shape[0], Q.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )  # (B, h, seq_len, d_k)
+        K = K.view(K.shape[0], K.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )  # (B, h, seq_len, d_k)
+        V = V.view(V.shape[0], V.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )  # (B, h, seq_len, d_k)
+
+        X, self.scores = MultiHeadAttention.attention(Q, K, V, mask, self.dropout)
+
+        X = (
+            X.transpose(1, 2).contiguous().view(X.shape[0], -1, self.d_model)
+        )  # (B, seq_len, d_model)
+
+        return self.w_o(X)  # (B, seq_len, d_model)
+
+    @staticmethod
+    def attention(
+        Q: Tensor, K: Tensor, V: Tensor, mask: Tensor, dropout: nn.Dropout
+    ) -> Tuple[Tensor, Tensor]:
+
+        d_k = Q.shape[-1]
+
+        scores = (Q @ K.transpose(-2, -1)) / math.sqrt(d_k)  # (B, h, seq_len, seq_len)
+
+        # Mask some values by a really small value to get the output of softmax near 0 for those values.
+        if mask:
+            scores.masked_fill_(mask == 0, -1e9)
+
+        scores = scores.softmax(dim=-1)  # (B, h, seq_len, seq_len)
+        if dropout:
+            scores = dropout(scores)
+
+        return scores @ V, scores  # (B, h, seq_len, d_k), (B, h, seq_len, seq_len)
+
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float = 0.1) -> None:
+        super().__init__()
+
+        self.dropout: nn.Module = nn.Dropout(dropout)
+        self.norm: nn.Module = LayerNorm()
+
+    def forward(self, X: Tensor, sublayer: Callable[[Tensor], Tensor]) -> Tensor:
+        # Most of online implementations online are doing the layer norm before the sublayer.
+        return X + self.dropout(sublayer(self.norm(X)))
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(
+        self,
+        attention: MultiHeadAttention,
+        feedforward: FeedForward,
+        dropout: float = 0.1,
+    ) -> None:
+        super().__init__()
+
+        self.attention: MultiHeadAttention = attention
+        self.res_attention: ResidualConnection = ResidualConnection(dropout)
+        self.feedforward: FeedForward = feedforward
+        self.res_feedforward: ResidualConnection = ResidualConnection(dropout)
+
+    def forward(self, X, mask: Tensor) -> None:
+
+        X = self.res_attention(X, lambda X: self.attention(X, X, X, mask))
+        X = self.res_feedforward(X, self.feedforward)
+
+        return X
+
+
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+
+        self.layers: nn.ModuleList[EncoderBlock] = layers
+        self.norm: LayerNorm = LayerNorm()
+
+    def forward(self, X: Tensor, mask: Tensor) -> Tensor:
+
+        for layer in self.layers:
+            X = layer(X, mask)
+
+        return self.norm(X)
 
 
 class Transformer(BaseTorchModel, BaseGenerativeModel):
